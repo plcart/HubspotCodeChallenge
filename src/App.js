2cbf0235019fb45bb87f1e9694cf6f55caad0083
Arthur Ferreira <arthur.ferreira@verizonconnect.com>
2020-03-22 13:33:42 +0000

    Exercise 1 - quote style

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+
+export default class extends React.Component {
+    render() {
+        return (<div className="App">
+            <h1>React Ready</h1>
+        </div>);
+    }
+
+}
